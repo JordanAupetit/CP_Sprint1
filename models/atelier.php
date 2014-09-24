a985124e1cbbb353afa8ebaf0574d5d3bbd63350
Jordan Aupetit <aupetit.jordan@gmail.com>
2014-09-24 16:53:17 +0200

    Debut du modele atelier

---
+++ models/atelier.php
@@ -0,0 +1,26 @@
+<?php
+
+class Atelier
+{
+	// Appel du script de connection à la BD -- a quel endroit exactement ?
+
+	public function add($title, $description) {
+
+	}
+
+	public function update_title($id, $title) {
+
+	}
+
+	public function update_description($id, $description) {
+
+	}
+
+	public function get_all_ateliers() {
+		// return array
+	}
+
+	public function remove($id) {
+
+	}
+}
