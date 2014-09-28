882204e4f7671cf194ac78d5bf3327635381abd0
sramanam <s_3055@live.fr>
2014-09-28 17:39:04 +0200

    atelierdetail
    afficher un atelier en particulier

---
+++ models/Atelierdetails.class.php
@@ -0,0 +1,36 @@
+<?php
+class AtelierDetails {
+protected $atelier;
+public function __construct($atelier) {
+	 $this->atelier = $atelier;
+  }
+
+
+  public function makeDetail() {
+	$nomAtelier = $this->atelier->getNomAtelier();
+	$dateAtelier = $this->atelier->getDateAtelier();
+    $description = $this->atelier->getDescription();
+    $id = $this->atelier->getId(); 
+
+       
+    $text = <<<EOT
+	<form  >
+
+	<div><span>Nom de l'atelier :</span>
+     	<span><input type="text" name="nomAtelier" value="{$nomAtelier}" disabled="disabled"  /></span>
+	</div>
+
+	<div><span>date :</span>
+     		<span><input type="text" name="dateAtelier" value="{$dateAtelier}" disabled="disabled"  /></span>
+	</div>
+
+	<div><span>Description :</span>
+     		<span><textarea name="description" cols="30" rows="3" disabled="disabled" >
+             		{$description}</textarea></span>
+	</div>
+
+	</form>
+EOT;
+return $text;
+ }
+ }
